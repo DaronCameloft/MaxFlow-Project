21c984dbc5a2aeaf1a2bc5794ed01d3b8090e423
DaronCameloft <daroncameloftdev@gmail.com>
2025-11-07 05:47:33 -0500

    Usar variables de entorno para la URL de la API_2

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -7,8 +7,14 @@ import axios from 'axios';
 import type { GraphInput, SolveResponse } from './types';
 
 // Creamos una "instancia" de axios con la URL base de tu backend
+// 1. Lee la variable de entorno que pusiste en Vercel
+const API_URL = import.meta.env.VITE_API_URL;
+
+// 2. Si la variable no existe (ej. en tu PC local), usa la URL local
+const FALLBACK_URL = 'http://127.0.0.1:8000/api';
+
 const apiClient = axios.create({
-  baseURL: 'https://maxflow-backend.onrender.com',
+  baseURL: `${API_URL}/api` || FALLBACK_URL, // Usa la variable de Vercel
 });
 
 // Creamos la función que llamará nuestro componente de React
